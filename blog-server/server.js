fb893b578a252b6e0d51a6127e859dd4396dbf46
Allen Ussher <allen@ussher.ca>
2019-05-13 21:06:04 -0700

    Add write API to post new blog posts

--- blog-server/server.js
+++ blog-server/server.js
@@ -2,16 +2,22 @@
 
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
 var posts = []
-for (var i=0; i < 3; i++) {
-    posts.push({ Title: `Title of Post ${i}`, Body: `Body of Post ${i}`, Delay: 0})
+for (var i=0; i < 5; i++) {
+    posts.push({ Title: `Post ${i}`, Body: `Body of Post ${i}`, Delay: 0})
 }
 
 // Make post 1 take 2s and post 2 5s
 posts[1].Delay = 2
 posts[2].Delay = 5
 
+// Posts 3 and 4 are special, so modify their titles
+posts[3].Title = "Post 3: Will fake 500"
+posts[4].Title = "Post 4: Will fake 404"
+
+app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
     console.log("%s %s %s\n", Date.now().toString(), req.originalUrl, JSON.stringify(req.headers))
@@ -19,12 +25,19 @@ app.use(function (req, res, next) {
 })
 
 app.get('/api/list', function (req, res) {
-    // Make a copy of posts that we will modify. We'll 
-    let postsPlusFake = [...posts]
-    postsPlusFake.push({ Title: "Title of Post 3: Will cause 500", Body: "Body will never be seen"})
-    postsPlusFake.push({ Title: "Title of Post 4: Will cause 404", Body: "Body will never be seen"})
+    res.json(posts)
+})
 
-    res.json(postsPlusFake)
+app.post('/api/write', function (req, res) {
+    let blogPost = req.body
+    // Modify it so we know server received it
+    blogPost.Title += " (server)"
+    blogPost.Body += " (server)"
+
+    posts.push(blogPost)
+
+    res.status(200)
+    res.send(blogPost)
 })
 
 app.get('/api/read/:id', function (req, res) {
@@ -37,28 +50,31 @@ app.get('/api/read/:id', function (req, res) {
         if (postId >= 0 && postId < posts.length) {
             // For testing, make post delay some time based on delay attribute
             setTimeout( () => {
-                // Only include Title and Body in the returned post. Ignore any other
-                // properties (such as delay).
-                let post = posts[postId]
-                let returnedPost = {Title: post.Title, Body: post.Body}
-                res.json(returnedPost)
+                // Special cases to fake errors
+                if (postId == 3) {
+                    res.status(500)
+                    res.send("500: Internal server error\n")
+                } else if (postId == 4) {
+                    res.status(404)
+                    res.send("404: Post not found\n")
+                } else {
+                    // Only include Title and Body in the returned post. Ignore any other
+                    // properties (such as delay).
+                    let post = posts[postId]
+                    let returnedPost = {Title: post.Title, Body: post.Body}
+                    res.json(returnedPost)
+                }
             }, posts[postId].Delay * 1000)
         } else {
-            // Special case, attempting to get post 3 will cause 500 status
-            if (postId == 3) {
-                res.status(500)
-                res.send("500: Internal server error\n")
-            } else {
-                res.status(404)
-                res.send("404: Post not found\n")
-            }
+            res.status(404)
+            res.send("404: Post not found\n")
         }
     }
 })
 
 app.get('/', function (req, res) {
 	res.send('Make post requests at /api/read/{postId}')
-    })
+})
 
 var server = app.listen(3000, function () {
 	var host = server.address().address
